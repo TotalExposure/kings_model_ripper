78d183ebea527ee760556ff0fac0f87527963103
John Cant <a.johncant@gmail.com>
2015-06-13 21:22:56 +0100

    Setup script

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='kcl_ripper',
+      version='0.1',
+      description='The funniest joke in the world. <-- I kept that in from the setup.py example',
+      url='http://github.com/TotalExposure/kings_model_ripper',
+      author='JohnCant',
+      author_email='a.johncant@gmail.com',
+      license='GPL',
+      install_requires=[
+          'Image',
+      ],
+      zip_safe=False)
